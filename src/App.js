a75759043dc4cde59b4f95b15642a0517eadd1b8
NosoaB17 <ngsn176@gmail.com>
2023-03-06 14:58:08 +0700

    Update Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import AddCustomerPage from "./pages/Customers/AddCustomerPage";
+import Customers from "./pages/Customers/Customers";
+import UpdateCustomerPage from "./pages/Customers/UpdateCustomerPage";
+import CustomersByHotel from "./pages/CustomersByHotel";
+import AddHotelPage from "./pages/Hotels/AddHotelPage";
+import Hotels from "./pages/Hotels/Hotels";
+import UpdateHotelPage from "./pages/Hotels/UpdateHotelPage";
+import AddBookingPage from "./pages/Reservations/AddBookingPage";
+import Bookings from "./pages/Reservations/Bookings";
+import UpdateBookingPage from "./pages/Reservations/UpdateBookingPage";
+import AddRoomPage from "./pages/Rooms/AddRoomPage";
+import Rooms from "./pages/Rooms/Rooms";
+import UpdateRoomPage from "./pages/Rooms/UpdateRoomPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/*" element={<Dashboard />}>
+          <Route path="*" element={<Hotels />} />
+
+          {/* hotels */}
+          <Route path="hotels" element={<Hotels />} />
+          <Route path="hotels/addHotels" element={<AddHotelPage />} />
+          <Route path="hotels/updateHotels" element={<UpdateHotelPage />} />
+          <Route path="hotels/updateHotels/:id" element={<UpdateHotelPage />} />
+
+          {/* Customers  */}
+          <Route path="customers" element={<Customers />} />
+          <Route path="customers/addCustomer" element={<AddCustomerPage />} />
+          <Route
+            path="customers/updateCustomer"
+            element={<UpdateCustomerPage />}
+          />
+          <Route
+            path="customers/updateCustomer/:id"
+            element={<UpdateCustomerPage />}
+          />
+
+          {/* Rooms  */}
+          <Route path="rooms" element={<Rooms />} />
+          <Route path="rooms/addRoom" element={<AddRoomPage />} />
+          <Route path="rooms/updateRoom" element={<UpdateRoomPage />} />
+          <Route path="rooms/updateRoom/:id" element={<UpdateRoomPage />} />
+
+          {/* Bookings  */}
+          <Route path="reservations" element={<Bookings />} />
+          <Route
+            path="reservations/addReservation"
+            element={<AddBookingPage />}
+          />
+          <Route
+            path="reservations/updateReservation"
+            element={<UpdateBookingPage />}
+          />
+          <Route
+            path="reservations/updateReservation/:id"
+            element={<UpdateBookingPage />}
+          />
+
+          {/* customer per hotel  */}
+          <Route path="customersPerHotel" element={<CustomersByHotel />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/FormButton.js
@@ -0,0 +1,25 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+function FormButton({ text }) {
+    return (
+        <Button variant="contained" 
+        color="success"
+        sx={{
+            background:'var(--primary)',
+            boxShadow:'none',
+            padding:'9px 26px',
+            textTransform:'capitalize',
+            '&:hover': {
+                boxShadow:'none'
+            }
+        }}
+        type="submit"
+        >
+        {text}
+        
+        </Button>
+    );
+}
+
+export default FormButton;
